08eff965985a51c1d84864bc0a4b3637ecf0317a
healLV <heal6@inbox.lv>
2019-09-19 15:29:29 +0300

    minor fix

--- commit.sh
+++ commit.sh
@@ -4,7 +4,7 @@ then
 	cd RTR105
 	git remote set-url origin git@github.com:okass/RTR105.git
 	git add .
-	git commit -a -m $1
+	git commit -a -m "$1"
 	git push
 else
 	echo Please provide a message
