6af7531940bb7a7c8b0d66d5bafaaa2bfa299279
healLV <heal6@inbox.lv>
2019-10-17 15:14:46 +0300

    lab 3 work finish

---
+++ lab_3/05.c
@@ -0,0 +1,19 @@
+/* Kristians Vigo Vilkins
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	printf("ievadi simbolu: ");
+	char abc;
+	scanf("%c", &abc);
+	
+	printf("%c \n", abc);
+	//1.3.2
+	printf("%d \n", abc);
+	//1.3.3
+	printf("%x \n", abc);
+	return 0;
+}
